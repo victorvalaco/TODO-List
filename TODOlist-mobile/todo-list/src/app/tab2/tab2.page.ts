7135120fbb611cb046f2ccddc88d8af27850243b
victorvalaco <cohetecoder@gmail.com>
2023-09-12 18:33:40 +0200

    feat(*): dark mode implemented

--- TODOlist-mobile/todo-list/src/app/tab2/tab2.page.ts
+++ TODOlist-mobile/todo-list/src/app/tab2/tab2.page.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
-  constructor() {}
+  themeToggle = false;
+  weightTextToggle = 'normal';
 
+  constructor() { }
+
+  ngOnInit() {
+    const prefersDark = window.matchMedia('(prefers-color-scheme: light)');
+
+    this.initializeDarkTheme(prefersDark.matches);
+
+    prefersDark.addEventListener('change', (mediaQuery) => this.initializeDarkTheme(mediaQuery.matches));
+  }
+
+  initializeDarkTheme(isDark: any) {
+    this.themeToggle = isDark;
+    this.toggleDarkTheme(isDark);
+  }
+
+  toggleChange(ev: any) {
+    this.toggleDarkTheme(ev.detail.checked);
+  }
+
+  toggleDarkTheme(shouldAdd: any) {
+    document.body.classList.toggle('dark', shouldAdd);
+  }
 }
